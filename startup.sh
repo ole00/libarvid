3e93c44cec0f43e7c174c2bc9068f8b05365a06c
ole <ole00@post.cz>
2015-08-31 17:30:15 +0000

    startup.sh: improved startup mechanism
    Now the arvid-server is run in a loop so it restarts
    automatically if a crash occurs. To stop the loop
    a file called 'stop' has to exist in the current
    directory.

--- startup.sh
+++ startup.sh
@@ -2,11 +2,32 @@
 
 #this is the statup script executed from /etc/rc.local
 
-#wait till everything is initialised
-sleep 12
+infinity=1
+
+#wait till everything is initialised & settled
+sleep 14
 
 #enter directory where pru files exist
 cd /home/debian/dev/libarvid
 
-#start the server
-./arvid-server
+#remove 'stop' file if it exists
+rm -f stop
+
+echo "Starting" > log.txt
+
+while [ $infinity -gt 0 ]; do
+	#start the server
+	./arvid-server >> log.txt
+
+	#if the server unexpectedly stopped, then start it again
+	#unless 'stop' file exists
+
+	#check whether to stop the loop if file 'stop' exists
+	if [ -e stop ]; then
+		echo "Stop found. Exiting..." >> log.txt
+		exit 0
+	else
+		sleep 2
+	fi
+done
+
